dfdf43a1881490bace7ef79ff9699f39a1ff02db
rcestep <roland.estep@outlook.com>
2020-02-29 22:40:54 -0500

    Update of automate.py

--- automate.py
+++ automate.py
@@ -2,7 +2,8 @@ def main():
 
     # variables being used
     hostname = ''
-    usernamw = ''
+    username = ''
+    password = ''
     verify = ''
 
     # This function will get the hostname
@@ -11,7 +12,13 @@ def main():
     # This function will get the username
     username = getUsername()
 
-    print(hostname,username)
+    # This function will get the password
+    password = getPassword()
+
+    # This function will get the type of interface
+    typeInterface = getTypeInterface()
+
+    print(hostname,username,password,typeInterface)
 ###########################################################
 # This function will ask for the hostname of the device
 def getHostname():
@@ -44,5 +51,43 @@ def getUsername():
             print('This is an invalid input!')
             verify = str(input('Are you sure this is the username you would like to use? ("y" for yes, "n" for no):  '))
     return username
+
+##########################################################
+# This function will ask the user for the password being used for their username
+def getPassword():
+    password = str(input('Please enter the password you would like to use for your username:  '))
+    verify = str(input('Are you sure this is the password you would like to use? ("y" for yes or "n" for no):  '))
+    while verify != 'y' and verify != 'n:
+        print('This is an invalid input!')
+        verify = str(input('Are you sure this is the password you would like to use? ("y" for yes or "n" for no):  '))
+    while verify == 'n':
+        password = str(input('Please enter the password you would like to use for your username:  '))
+        verify = str(input('Are you sure this is the password you would like to use? ("y" for yes or "n" for no):  '))
+        while verify != 'y' and verify != 'n:
+            print('This is an invalid input!')
+            verify = str(input('Are you sure this is the password you would like to use? ("y" for yes or "n" for no):  '))
+    return password
+
+##########################################################
+# This function will get the interface that will be configured
+def getTypeInterface():
+    typeInterface = str(input('Please enter the interface you would like to configure ("g" for GigabitEthernet, "f" for FastEthernet, "lo" for loopback, or "s" for serial):  '))
+    while typeInterface != 'g' and typeInterface != 'f' and typeInterface != 'lo' and typeInterface != 's':
+        print('Invalid type of interface!')
+        typeInterface = str(input('Please enter the interface you would like to configure ("g" for GigabitEthernet, "f" for FastEthernet, "lo" for loopback, or "s" for serial):  '))
+    verify = str(input('Are you sure this is the interface you want to configure? ("y" for yes or "n" for no):  '))
+    while verify != 'y' and verify != 'n':
+        print('This is an invalid input!')
+        verify = str(input('Are you sure this is the interface you want to configure? ("y" for yes or "n" for no):  '))
+    while verify == 'n':
+        typeInterface = str(input('Please enter the interface you would like to configure ("g" for GigabitEthernet, "f" for FastEthernet, "lo" for loopback, or "s" for serial):  '))
+        while typeInterface != 'g' and typeInterface != 'f' and typeInterface != 'lo' and typeInterface != 's':
+            print('Invalid type of interface!')
+            typeInterface = str(input('Please enter the interface you would like to configure ("g" for GigabitEthernet, "f" for FastEthernet, "lo" for loopback, or "s" for serial):  '))
+        verify = str(input('Are you sure this is the interface you want to configure? ("y" for yes or "n" for no):  '))
+        while verify != 'y' and verify != 'n':
+            print('This is an invalid input!')
+            verify = str(input('Are you sure this is the interface you want to configure? ("y" for yes or "n" for no):  '))
+    return typeInterface
 
 main()
